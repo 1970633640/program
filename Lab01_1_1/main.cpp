1e706bbc3d2544ab693e444f14bea18587ad45cf
bgtest001 <q1970633640@gmail.com>
2016-09-06 11:01:02 +0800

    123

--- Lab01_1_1/main.cpp
+++ Lab01_1_1/main.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
-
+#include <stdio.h>
+#include <stdlib.h>
 using namespace std;
 
-int main()
+int main()  //²åÈëÅÅĞòÉıĞòÊä³ö
 {
-    cout << "Hello world!" << endl;
+    freopen ("data1w.txt", "r", stdin);
+    freopen ("output.txt", "w", stdout);
+    int temp,key,x,i,j;
+    cin>>x;
+    int   *ans=new   int[x];
+    for(i=0; i<x; i++)
+    {
+        cin>>key;
+        ans[i]=key;
+    }
+    for(i=1; i<x; i++)
+    {
+        if(ans[i]<ans[i-1])
+        {
+            temp=ans[i];
+            j=i;
+            ans[j]=ans[j-1];
+            while(j>0 && temp<ans[j])
+            {
+                j--;
+                ans[j]=ans[j-1];
+            }
+            ans[j]=temp;
+        }
+    }
+    for(i=0; i<x; i++)cout<<ans[i]<<endl;
     return 0;
-
 }
