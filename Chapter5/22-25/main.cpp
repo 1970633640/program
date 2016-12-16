1059742b035e37ccafd3641e4243b0f2f248b0d4
1970633640 <q1970633640@gmail.com>
2016-12-16 19:58:01 +0800

    123

--- Chapter5/22-25/main.cpp
+++ Chapter5/22-25/main.cpp
@@ -13,9 +13,10 @@ typedef struct node2
     node2* lchild;
     node2* rchild;
     char value;
-}node2;
+} node2;
 
-node * T[3];node2 * TT;
+node * T[3];
+node2 * TT;
 int sum;
 char x;
 node * create_tree() //建立四叉树
@@ -72,30 +73,45 @@ node * create_tree() //建立四叉树
     }
     return ans;
 }
-node2 * beta(node * p ,node * q,node * r,node * s)
+node2 * beta(node * p,node * q,node * r,node * s)
 {
-node2 * t;
-if(p==NULL)return NULL;
-t=new node2;
-t->value=p->value;
-t->lchild=beta(p->child[0],p->child[1],p->child[2],p->child[3]);
-t->rchild=beta(q,r,s,NULL);
-return t;
+    node2 * t;
+    if(p==NULL)return NULL;
+    t=new node2;
+    t->value=p->value;
+    t->lchild=beta(p->child[0],p->child[1],p->child[2],p->child[3]);
+    t->rchild=beta(q,r,s,NULL);
+    return t;
+}
+
+node2 * copy(node2 * t)
+{
+    if(t==NULL)return NULL;
+    else
+    {
+        node2 * p=new node2;
+        p->value=t->value;
+        p->lchild=copy(t->lchild);
+        p->rchild=copy(t->rchild);
+        return p;
+    }
 }
 
 void s_midorder(node2 * root)
 {
-    stack <node2*> STACK;node2 * p=root;
+    stack <node2*> STACK;
+    node2 * p=root;
     while(!STACK.empty() ||p)
     {
-while(p!=NULL)
-{
-    STACK.push(p);
-    p=p->lchild;
-}
-p=STACK.top();STACK.pop();
-printf("%c",p->value);
-p=p->rchild;
+        while(p!=NULL)
+        {
+            STACK.push(p);
+            p=p->lchild;
+        }
+        p=STACK.top();
+        STACK.pop();
+        printf("%c",p->value);
+        p=p->rchild;
     }
 }
 
@@ -103,32 +119,35 @@ void s_posorder(node2 * root)
 {
     stack <node2*> stack1,stack2;
     node2 * p;
-stack1.push(root);
-while(!stack1.empty())
-{
-    p=stack1.top();stack1.pop();
-    stack2.push(p);
-    if(p->lchild!=NULL)stack1.push(p->lchild);
-     if(p->rchild!=NULL)stack1.push(p->rchild);
+    stack1.push(root);
+    while(!stack1.empty())
+    {
+        p=stack1.top();
+        stack1.pop();
+        stack2.push(p);
+        if(p->lchild!=NULL)stack1.push(p->lchild);
+        if(p->rchild!=NULL)stack1.push(p->rchild);
+    }
+    while(!stack2.empty())
+    {
+        printf("%c",stack2.top()->value);
+        stack2.pop();
+    }
 }
-while(!stack2.empty())
-{
-    printf("%c",stack2.top()->value);stack2.pop();
-
-}
-}
-
 
 int main()
 {
-   for(int i=0;i<3;++i)
- {
-   T[i]=create_tree();if(x=='\n')break;
-   }
+    for(int i=0; i<3; ++i)
+    {
+        T[i]=create_tree();
+        if(x=='\n')break;
+    }
     TT=beta(T[0],T[1],T[2],T[3]);
-
     s_midorder(TT);
     printf("\n");
-s_posorder(TT);
+    s_posorder(TT);
+    printf("\n");
+    node2 * t0=copy(TT);
+    s_posorder(t0);
     return 0;
 }
---
+++ bigdata1/main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+//#include <pthread.h> //多线程相关操作头文件，可移植众多平台
+#include <stdio.h>
+#include <cstring>
+#include<windows.h>
+//#include <hash_set>
+//#include <array>
+#include <fstream>
+using namespace std;
+string query[30]; //允许最大关键词=30
+char cquery[30][15]; //char版查询词
+int querysum=-1; //从零开始
+#define filesum 101710//元素总数钦定0~101710
+const int MAXN = 20000;//钦定文件最大20000字节
+//#define filesum 1000
+bool ans[30][filesum];
+bool ansfind[30];
+char fname[48];//小文件名字
+
+int ansfindsum;
+int len;//小文件长度
+int main()
+{
+    long long int c;
+    int cc;
+    ifstream qin;
+    qin.open("query_task1.txt");
+    string x;
+    while(qin>>x)
+    {
+        query[++querysum]=x;
+        strcpy(cquery[querysum],query[querysum].c_str());
+    }
+//cout<<"all-1="<<querysum;
+    memset(ans,0,sizeof(ans));
+    FILE* fin;
+//char* buf=new char[MAXN];//文件所有字符
+    char com[30];
+    for(int i=0; i<=filesum; ++i)
+    {
+        if(i%10000==0)printf("%d\n",i);
+        sprintf(fname,"D:\\program\\bigdata\\processed\\report%d.xml",i);
+        //  printf("%s\n",fname);
+        fin=fopen(fname,"rb");
+        if(fin==NULL) printf("NULL\n");
+        //          fseek (fin , 0 , SEEK_END);
+//   len = ftell (fin);
+//   rewind (fin);
+//fread(buf,1,len,fin);
+//buf[len]='\0';
+        //  fclose(fin);
+
+        memset(ansfind,0,sizeof(ansfind));
+        ansfindsum=-1;
+
+        while(!feof(fin))
+        {
+fscanf(fin,"%s",com);
+                for(int j=0; j<=querysum; ++j) //j=0-29
+                    if(!ansfind[j] && strcmp( com,cquery[j] )==0 )
+                    {
+                        ans[j][i]=true;
+                        ++ansfindsum;
+                        ansfind[j]=true;
+                        if(ansfindsum==querysum)goto eee;
+                    }
+            }
+
+eee:
+        fclose(fin);
+    }
+
+    ofstream fout;
+    fout.open("Task1.txt");
+    DWORD start_time=GetTickCount();
+    int i,j;
+    for(i=0; i<=querysum; ++i)
+    {
+        string qs=query[i];
+        for( j=0; j<=filesum; ++j)
+            if(ans[i][j])fout<<i+1<<"  report"<< j <<".xml "<<qs<<endl;
+    }
+    DWORD end_time=GetTickCount();
+    cout<<"The output time is:"<<(end_time-start_time)<<"ms!"<<endl;//输出运行时间
+    fout.close();
+//delete []buf;
+    return 0;
+}
